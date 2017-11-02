8fc5af69f4f451e8735426f909652c5d22122e39
hemalijain <hemalijains@gmail.com>
2017-11-02 17:00:11 +0530

    change type

--- db/migrate/20171031083404_changecolumntype.rb
+++ db/migrate/20171031083404_changecolumntype.rb
@@ -1,5 +1,5 @@
 class Changecolumntype < ActiveRecord::Migration[5.1]
   def change
-    change_column :friends,:reminder,'integer USING CAST(column_name AS integer)'
+    change_column :friends,:reminder,'integer USING CAST(reminder AS integer)'
   end
 end
